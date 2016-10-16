3a4b4a74d4f3f17fd7de5f142201578fb43ac817
bjonness406 <andreasbjonness@hotmail.com>
2016-10-16 10:10:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
  curl https://gitlab.com/ThatGuy/convert2mkv/raw/master/convert2mkv.sh > /config/convert2mkv.sh && \
  chmod +x /config/convert2mkv.sh
  
- convert2mkv.sh
+ sh /config/convert2mkv.sh
