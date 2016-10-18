d91a54110b012655b1d74c2cb8e1d99ac4b156e4
bjonness406 <andreasbjonness@hotmail.com>
2016-10-18 16:10:33 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+echo "\n \n \n ------------------------Starting container ------------------------ \n \n \n"
+
+if [[ ! -f "/config/convert2mkv.sh" ]]; then
+ echo "[Info] Script already downloaded, skipping download. If you want to redownload the script, delete convert2mkv.sh in you appdata"
+
+else
+ echo "[Info] Downloading script..."
  curl https://gitlab.com/ThatGuy/convert2mkv/raw/master/convert2mkv.sh > /config/convert2mkv.sh && \
  chmod +x /config/convert2mkv.sh
  
+ fi
  bash /config/convert2mkv.sh
  
- #Only for testing, when docker will not stop immediately after script.. 
- sleep 300
+Echo "Stopping Container, script finished.."
