5267ae26489c6b683ef2217440ff7fced5ca595f
bjonness406 <andreasbjonness@hotmail.com>
2016-10-18 15:45:42 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
  curl https://gitlab.com/ThatGuy/convert2mkv/raw/master/convert2mkv.sh > /config/convert2mkv.sh && \
  chmod +x /config/convert2mkv.sh
  
- sh /config/convert2mkv.sh
+ bash /config/convert2mkv.sh
  
  #Only for testing, when docker will not stop immediately after script.. 
  sleep 300
