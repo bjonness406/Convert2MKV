da3492d00d99a5735e39c442953473c6a65d1644
bjonness406 <andreasbjonness@hotmail.com>
2016-10-18 16:46:58 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+#Give message when starting the container
 printf "\n \n \n ------------------------Starting container ------------------------ \n \n \n"
 
-if [[ ! -f "/config/convert2mkv.sh" ]]; then
+#If convert2mkv.sh exist, then skip redownloading it.
+if [[ -f "/config/convert2mkv.sh" ]]; then
+ 
  echo "[Info] Script already downloaded, skipping download. If you want to redownload the script, delete convert2mkv.sh in you appdata"
 
+#Downloading script if it is not found in /config
 else
  echo "[Info] Downloading script..."
  curl https://gitlab.com/ThatGuy/convert2mkv/raw/master/convert2mkv.sh > /config/convert2mkv.sh && \
